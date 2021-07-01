5aa0d791a5c9fbbfac1f9d1ca48c9a41421b39f3
mahmoud77732 <mm8871918@gmail.com>
2021-07-01 22:39:16 +0200

    add home page

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+    $(".home .img-4").css("left", "120px");
+    $(".home .img-4").css("opacity", "1");
+    $(".home .img-5").css("right", "60px");
+    $(".home .img-5").css("opacity", "1");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
